a1fcd451fa3577675753af037e39001919038e55
Ankit Kafle <notankitff@gmail.com>
2026-07-05 21:39:32 +0545

    Finally I have started For Project slowly

---
+++ Project/proj02/proj02/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+
+
+const router = createBrowserRouter(
+  [
+
+{
+  path:"/",
+  element:
+  <div>
+
+  </div>
+},
+{
+    path:"/pastes",
+  element:
+  <div>
+    
+  </div>
+},
+{
+    path:"/pastes/id:",
+  element:
+  <div>
+    
+  </div>
+}
+
+  ]
+)
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
