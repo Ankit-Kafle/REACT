414c5a01fc996b507d3e9d9fa25c0eb5227544de
Ankit Kafle <notankitff@gmail.com>
2026-07-07 18:30:59 +0545

    Bored

--- Project/proj02/proj02/src/components/Home.jsx
+++ Project/proj02/proj02/src/components/Home.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react"
+import React from "react"
+import { useSearchParams } from "react-router-dom";
+
 const Home = () => {
+
+  const [title,setTitle]=useState('');
+  const [value,setValue]= useState('');
+  const [searchParams,setSearchParams] = useSearchParams('');
+  const pasteId= searchParams.get("pasteId")
   return (
     <div>
+      <input
       
+      className='p-3 rounded-medium mt-2 '
+      type='text '
+      placeholder='enter title here'
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}
+      
+      />
+
+      <button>
+
+    {
+      pasteId ? "Update my paste":"Create my paste"
+    } 
+            
+      </button>
     </div>
   )
 }
