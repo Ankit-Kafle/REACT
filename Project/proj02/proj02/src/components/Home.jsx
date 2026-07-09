e334bf9afe3c39b2f44ef8319b96ccb24bf6e08b
Ankit Kafle <notankitff@gmail.com>
2026-07-09 21:23:34 +0545

    Gaurango

--- Project/proj02/proj02/src/components/Home.jsx
+++ Project/proj02/proj02/src/components/Home.jsx
@@ -1,34 +1,48 @@
-import { useState } from "react"
-import React from "react"
+import React, { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 
 const Home = () => {
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState("");
 
-  const [title,setTitle]=useState('');
-  const [value,setValue]= useState('');
-  const [searchParams,setSearchParams] = useSearchParams('');
-  const pasteId= searchParams.get("pasteId")
+  const [searchParams] = useSearchParams();
+
+  const pasteId = searchParams.get("pasteId");
+
   return (
-    <div>
+  <div>
+      <div className="flex flex-row gap-7">
       <input
-      
-      className='p-3 rounded-medium mt-2 '
-      type='text '
-      placeholder='enter title here'
-      value={title}
-      onChange={(e) => setTitle(e.target.value)}
-      
+        className="p-3 rounded-medium mt-2"
+        type="text"
+        placeholder="Enter title here"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
 
       <button>
-
-    {
-      pasteId ? "Update my paste":"Create my paste"
-    } 
-            
+        {pasteId ? "Update my paste" : "Create my paste"}
       </button>
     </div>
-  )
-}
 
-export default Home
+
+    <div className="mt-8">
+
+      <textarea
+      className="rounded-2xl mt-4,min-w-[500px] p-4"
+
+      value={value}
+      placeholder=" Enter Content Here"
+
+      onChange={(e) => setValue(e.target.value)}
+      rows={20}
+      />
+
+
+
+    </div>
+  </div>
+  );
+};
+
+export default Home;
